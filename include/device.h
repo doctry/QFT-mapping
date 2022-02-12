6e5dfa179100f7c503730d78907cf23fddd9c6b7
doctry <jonathanyang475869@gmail.com>
2022-02-12 17:21:35 +0800

    finish before algorithm

--- include/device.h
+++ include/device.h
@@ -13,11 +13,13 @@ namespace device
         Qubit(Qubit &&other);
 
         void add_adj(unsigned i);
+        unsigned get_avail_time() const;
 
     private:
         unsigned _id;
         std::vector<unsigned> _adj_list;
         unsigned _topo_qubit;
+        unsigned _occupied_until;
     };
 
     class Device
@@ -27,6 +29,8 @@ namespace device
         Device(const Qubit &other) = delete;
         Device(Device &&other);
 
+        const unsigned get_num_qubits() const;
+        Qubit &get_qubit(const unsigned i);
     protected:
         std::vector<Qubit> _qubits;
     };
--- include/qft_topo.h
+++ include/qft_topo.h
@@ -1,3 +1,5 @@
+#include <vector>
+
 #pragma once
 
 namespace topo
@@ -10,26 +12,37 @@ namespace topo
         Qubit(Qubit &&other);
 
     private:
+        unsigned _id;
+        unsigned _location;
     };
 
     class Gate
     {
     public:
-        Gate(const unsigned i);
+        Gate(const unsigned q0, const unsigned q1);
         Gate(const Gate &other) = delete;
         Gate(Gate &&other);
 
     private:
         unsigned _q0;
         unsigned _q1;
-        unsigned _next_gate;
     };
 
-    class DependencyGraph
+    class QFTTopology
     {
     public:
-        DependencyGraph();
-        DependencyGraph(const DependencyGraph &other) = delete;
-        DependencyGraph(DependencyGraph &&other);
+        QFTTopology(unsigned num);
+        QFTTopology(const QFTTopology &other) = delete;
+        QFTTopology(QFTTopology &&other);
+
+        const unsigned get_num_qubits() const;
+        Gate &pop_next_gate();
+        Gate &get_next_gate();
+        Qubit &get_qubit(const unsigned i);
+
+    private:
+        std::vector<Gate> _gates;
+        unsigned _next_gate_idx;
+        std::vector<Qubit> _qubits;
     };
 }
