3167e0f1fbd1c50bdda3616b39676564cb3faa4c
doctry <jonathanyang475869@gmail.com>
2022-03-17 23:24:42 +0800

    convert device

--- eval/convert_device.py
+++ eval/convert_device.py
@@ -1,4 +1,11 @@
 from test_device import Device
+import sys
+import json
 
-if __name__ == "main":
-    device = Device()
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    device = Device(filename)
+    output = device.to_json()
+
+    output_file = open(sys.argv[2], 'w')
+    json.dump(output, output_file)
