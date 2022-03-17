a3e548623ba570bfd3286684f18d3ea524e27669
doctry <jonathanyang475869@gmail.com>
2022-03-18 00:30:57 +0800

    test_output finished not tested

--- eval/test_output.py
+++ eval/test_output.py
@@ -1,17 +1,62 @@
 import json
 import sys
+from numpy import sort
 
 from rich import traceback
-from test_device import Device, Operation
-from test_topo import QFTTopo
+from test_device import Device, Operation, Qubit
+from test_topo import QFTTopo, Gate
 
 traceback.install()
 
 
-def test_operations(operations: list[Operation], qft_topo: QFTTopo, device: Device):
-    # for op in operations:
+class Checker:
+    def __init__(
+        self,
+        device: Device,
+        qft_topo: QFTTopo,
+        operations: list[Operation],
+        cycle: dict,
+    ):
+        self.device: Device = device
+        self.qft_topo: QFTTopo = qft_topo
+        self.operations: list[Operation] = operations
+        self.cycle: dict = cycle
 
-    return operations[-1].duration[1]
+    def apply_gate(self, op: Operation, gate: Gate) -> bool:
+        (d_q0_idx, d_q1_idx) = op.qubits
+        d_q0 = self.device.qubits[d_q0_idx]
+        d_q1 = self.device.qubits[d_q1_idx]
+        topos: list[int] = [d_q0.topo, d_q1.topo]
+        sort(topos)
+        if topos != gate.qubits:
+            return False
+
+        (start, end) = op.duration
+        assert start >= d_q0.occupied_until and start >= d_q1.occupied_until, (
+            start,
+            d_q0.occupied_until,
+            d_q1.occupied_until,
+        )
+        assert end == start + self.cycle[op.operator], (end, start, op.operator)
+        d_q0.occupied_until = end
+        d_q1.occupied_until = end
+
+        if op.operator == "Swap":
+            d_q0.topo, d_q1.topo = d_q1.topo, d_q0.topo
+
+        return True
+
+    def test_operations(self) -> int:
+        for op in self.operations:
+            avail_gates = self.qft_topo.avail_gates
+            pass_condition = False
+            for g in avail_gates:
+                if self.apply_gate(op, self.qft_topo.gates[g]):
+                    pass_condition = True
+                    qft_topo.update_avail_gates(g)
+            assert pass_condition
+
+        return self.operations[-1].duration[1]
 
 
 if __name__ == "__main__":
@@ -29,16 +74,18 @@ if __name__ == "__main__":
     R_cycle = int(sys.argv[4])
     Swap_cycle = int(sys.argv[5])
 
-    cycle = {"R": R_cycle, "Swap": Swap_cycle}
+    cycle:dict[int] = {"R": R_cycle, "Swap": Swap_cycle}
 
     operations = output["Operations"]
-    final_cost = output["final_cost"]
+    final_cost: int = output["final_cost"]
 
-    operations = [
+    operations: list[Operation] = [
         Operation(o["Operator"], tuple(o["Qubits"]), tuple(o["duration"]))
         for o in operations
     ]
 
-    test_cost = test_operations(operations, qft_topo, device)
+    checker = Checker(device, qft_topo, operations, cycle)
+
+    test_cost = checker.test_operations()
 
     assert test_cost == final_cost, (test_cost, final_cost)
