23236d333a549e5e04a3dbbdb318d303d9c9b860
RenChu Wang <patrick1031wang@gmail.com>
2022-03-29 12:34:34 +0800

    mid paths and swap done.

--- max_short_paths/graph/dev.py
+++ max_short_paths/graph/dev.py
@@ -1,18 +1,21 @@
 from __future__ import annotations
 
+import itertools
 import json
 from collections import defaultdict
 from pathlib import Path
-from typing import Any, Dict, List, Sequence, Tuple
+from typing import Any, Dict, Iterable, List, Sequence, Tuple
 
 import loguru
 import networkx as nx
 import numpy as np
 from networkx import Graph
+from numpy import ndarray
 from scipy.sparse import csgraph
 from typing_extensions import Self
 
 from max_short_paths.interfaces import SerDeGraph
+from .dep import QuBitOp
 
 
 class DeviceGraph(Graph, SerDeGraph):
@@ -41,7 +44,10 @@ class DeviceDriver:
         self.mapping = {i: i for i in range(len(device.nodes))}
         self.paths = self._init_paths()
         self.distance = csgraph.shortest_path(nx.to_scipy_sparse_array(self.device))
+
         self.midpoints = self._init_midpoints()
+
+        self.midpaths = self._init_midpaths()
         self.middist = self._get_midpoint_dist()
 
         assert not self.device.is_directed()
@@ -56,7 +62,9 @@ class DeviceDriver:
     def __str__(self) -> str:
         return str(self.to_json())
 
-    def path(self, source: int, target: int) -> Sequence[int]:
+    def perceived_distance(self, op: QuBitOp) -> Sequence[int]:
+        source = self.mapping[op.source]
+        target = self.mapping[op.target]
         return self.paths[source, target]
 
     def to_json(self):
@@ -65,23 +73,52 @@ class DeviceDriver:
             "mapping": self.mapping,
             "paths": self.paths,
             "distance": self.distance.tolist(),
-            "midpoints": self.midpoints,
+            "midpoints": self.midpoints.tolist(),
+            "middist": self.middist,
+            "midpaths": self.midpaths,
         }
 
-    def rotate(self, rotation: List[int]) -> List[int]:
+    def _rotate(self, rotation: List[int], first: bool = True) -> List[int]:
+        "Rotates the qubits but also update mapping."
+
         if not rotation:
             return []
 
-        first = rotation[0]
-        shifted = rotation[1:]
-        shifted.append(first)
-        return shifted
+        if first:
+            head = rotation[0]
+            shifted = rotation[1:]
+            shifted.append(head)
+            return shifted
+        else:
+            head = rotation[-1]
+            return [head] + rotation[:-1]
+
+    def rotate(self, rotation: List[int], first: bool) -> None:
+        "Rotate and update mapping."
+
+        if not rotation:
+            return
+
+        rotated = self._rotate(rotation, first=first)
+        assert len(rotation) == len(rotated), [rotation, rotated]
+
+        for (f, t) in zip(rotation, rotated):
+            self.mapping[f] = t
+
+    def swap(self, op: QuBitOp) -> None:
+        (source, target) = op
+
+        source2mid = self.midpaths[source, target]
+        target2mid = self.midpoints[target, source]
+
+        self.rotate(source2mid, first=False)
+        self.rotate(target2mid, first=False)
 
     def _init_paths(self) -> Dict[Tuple[int, int], List[int]]:
         shortest_paths = nx.shortest_path(self.device)
         loguru.logger.debug("Shortest paths: {}", shortest_paths)
 
-        dd = defaultdict(list)
+        dd = {}
 
         for (source, target_path) in shortest_paths.items():
             for (target, path) in target_path.items():
@@ -122,8 +159,9 @@ class DeviceDriver:
             f"Unreachable point. Distance matrix should be all positive: {self.distance}"
         )
 
-    def _init_midpoints(self) -> Dict[Tuple[int, int], Tuple[int, int]]:
-        results = {}
+    def _init_midpoints(self) -> ndarray:
+        dim = len(self.device.nodes)
+        results = np.full([dim, dim, 2], -1)
 
         for ((source, target), path) in self.paths.items():
             assert path[0] == source, [source, path]
@@ -134,15 +172,41 @@ class DeviceDriver:
 
         return results
 
-    def _get_midpoint_dist(self) -> np.ndarray:
+    def _get_midpoint_dist(self) -> ndarray:
         dim = len(self.device.nodes)
         dd = np.full([dim, dim], np.inf)
 
-        for ((source, target), (mid_src, mid_tgt)) in self.midpoints.items():
-            smaller = max(
-                self.distance[source, mid_src], self.distance[mid_tgt, target]
-            )
-            dd[source, target] = smaller
-            dd[target, source] = smaller
+        (a, b, c) = self.midpoints.shape
+        assert a == b == dim, [a, b, dim]
+        assert c == 2, c
+
+        for (source, target) in itertools.product(range(dim), range(dim)):
+            (mid_src, mid_tgt) = self.midpoints[source, target]
+            larger = max(self.distance[source, mid_src], self.distance[mid_tgt, target])
+            dd[source, target] = dd[target, source] = larger
 
         return dd
+
+    def _init_midpaths(self) -> Dict[Tuple[int, int], List[int]]:
+        def _collect_until(it: Iterable[int], target: int) -> List[int]:
+            result = []
+
+            for i in it:
+                result.append(i)
+
+                if i == target:
+                    return result
+
+            raise ValueError("Iterable does not contain target")
+
+        results = {}
+
+        for ((source, target), paths) in self.paths.items():
+
+            (mids, midt) = self.midpoints[source, target]
+            assert mids in paths, [mids, paths]
+            assert midt in paths, [midt, paths]
+            results[source, target] = _collect_until(paths, mids)
+            results[target, source] = _collect_until(reversed(paths), midt)
+
+        return results
