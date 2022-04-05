f35b2a88d54230e1b7557c3857f6af423575d8b3
renchu <rentrue@proton.me>
2022-04-05 23:56:56 +0800

    merge device and driver

--- qft/devices/interfaces.py
+++ qft/devices/interfaces.py
@@ -1,11 +1,24 @@
+import copy
 from abc import abstractmethod
-from typing import Protocol
+from typing import List, Protocol
 
 from networkx import Graph
 
 
 class Device(Protocol):
-    @property
-    @abstractmethod
-    def device(self) -> Graph:
-        ...
+    device: Graph
+    mapping: List[int]
+
+    def rotate(self, indices: List[int], *, right: bool) -> None:
+        rotated = self._rotate(indices, right=right)
+        cloned_map = copy.copy(self.mapping)
+
+        for (idx, ridx) in zip(indices, rotated):
+            self.mapping[idx] = cloned_map[ridx]
+
+    @staticmethod
+    def _rotate(indices: List[int], *, right: bool) -> List[int]:
+        if right:
+            return indices[-1:] + indices[:-1]
+        else:
+            return indices[1:] + indices[:1]
--- qft/drivers/__init__.py
+++
@@ -1,2 +0,0 @@
-from .interfaces import DeviceDriver
-from .states import VanillaState
