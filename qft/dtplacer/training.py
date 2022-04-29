bba408552151f388e0571e6c06372bd7d736f371
doctry <jonathanyang475869@gmail.com>
2022-04-29 18:28:48 +0800

    gym env part

---
+++ qft/dtplacer/training.py
@@ -0,0 +1,14 @@
+from gym import Env, spaces
+import numpy as np
+
+class RoutingEnv(Env):
+    def __init__(self, num_qubit: int):
+        super(RoutingEnv, self).__init__()
+
+        # observation space
+        self.observation_shape = 3*num_qubit
+        self.observation_space = spaces.Box(low = np.zeros(self.observation_shape), 
+                                            high = np.ones(self.observation_shape),
+                                            dtype = np.float16)
+
+        
