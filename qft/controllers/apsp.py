3ca7817d9535e147f695da0d72ed96dc976e2056
renchu <rentrue@proton.me>
2022-04-29 23:44:07 +0800

    schedule the shortest paths

--- qft/controllers/apsp.py
+++ qft/controllers/apsp.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 
 from typing import List, Tuple
 
-from qft.common import CompiledOp
+from qft.common import CompiledOp, QubitOp
+from qft.deps.interfaces import Consumer, Dependency
 from qft.devs import Device
 from qft.routers import Router
 from qft.schedulers import Scheduler
@@ -10,18 +11,9 @@ from qft.schedulers import Scheduler
 from .interfaces import Controller, Timing
 
 
-class APSPController(Controller):
-    def __init__(
-        self, device: Device, scheduler: Scheduler, router: Router, timing: Timing
-    ) -> None:
-        super().__init__()
-
-        self.device = device
-        self.scheduler = scheduler
-        self.router = router
-        self.timing = timing
-
-        self.available = [0] * len(self.device.g.nodes)
+class APSPMixin:
+    timing: Timing
+    available: List[int]
 
     def compile_route(self, route: Tuple[List[int], List[int]]) -> List[CompiledOp]:
         (r0, r1) = route
@@ -45,3 +37,40 @@ class APSPController(Controller):
         end = start + self.timing.operation
         self.available[a] = self.available[b] = end
         return CompiledOp(operator="R", physical=(a, b), duration=(start, end))
+
+
+class APSPController(APSPMixin, Controller):
+    def __init__(
+        self, device: Device, scheduler: Scheduler, router: Router, timing: Timing
+    ) -> None:
+        self.device = device
+        self.scheduler = scheduler
+        self.router = router
+        self.timing = timing
+        self.available = [0] * len(self.device.g.nodes)
+
+
+class WorkStealingController(APSPMixin, Scheduler):
+    def __init__(
+        self, device: Device, dependency: Dependency, router: Router, timing: Timing
+    ) -> None:
+        self.device = device
+        self.dependency = dependency
+        self.consumer = self.dependency.consumer()
+        self.router = router
+        self.timing = timing
+        self.available = [0] * len(self.device.g.nodes)
+
+    @property
+    def scheduler(self) -> Scheduler:
+        return self
+
+    def next_op(self) -> QubitOp:
+        ready = tuple(self.consumer.ready)
+        shortest = min(ready, key=lambda x: len(self.router.route(x.source, x.target)))
+        self.consumer.process(shortest)
+        return shortest
+
+    @property
+    def done(self) -> bool:
+        return not len(self.consumer)
