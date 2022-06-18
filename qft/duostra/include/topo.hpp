f3713050e5f58aff0064ec8039e1f550a02d7055
RenChu Wang <patrick1031wang@gmail.com>
2022-06-18 13:55:45 +0800

    just going to temporarily leave it here until i figure out what the second bool in prevs is

--- qft/duostra/include/topo.hpp
+++ qft/duostra/include/topo.hpp
@@ -57,7 +57,7 @@ class Topology {
   public:
     virtual unsigned get_num_qubits() const = 0;
     virtual unsigned get_num_gates() const = 0;
-    virtual Gate &get_gate(const unsigned i) = 0;
+    virtual Gate &get_gate(unsigned i) = 0;
     virtual const std::vector<unsigned> &get_avail_gates() const = 0;
     virtual void update_avail_gates(unsigned executed) = 0;
 };
