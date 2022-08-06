9217a0081fbc0e6b9a662fd951f11bec259de58f
chinyi0523 <james31423a@gmail.com>
2022-08-06 21:49:22 +0800

    :wheelchair: Make scripts more friendly

--- utils/run_benchmark.sh
+++ utils/run_benchmark.sh
@@ -5,18 +5,21 @@ export OMP_NUM_THREADS=8
 
 repo_root='/home/chinyi0523/QFT-mapping'
 data_root='/home/chinyi0523/QFT-mapping/benchmark/original/large/'
-depth=1
+depth=5
 candidates=-1
 exec_single=false
 
-echo "Results to benchmarks" > result.log
-echo "Running times to benchmarks" > time.log
+result_f=result_nonexec_5.log
+time_f=time_nonexec_5.log
 
-for name in 9symml_195 cycle10_2_110 inc_237 d53_251 sqrt8_260 z4_268 adr4_197 dc2_222 life_238 rd73_252 square_root_7 cm42a_207 dist_223 mlp4_245 rd84_253 urf1_149 cm82a_208 ham15_107 pm1_249 root_255 urf1_278 cm85a_209 hwb8_113 qft_10 sqn_258 urf2_277; do
+echo "Results to benchmarks" > $result_f
+echo "Running times to benchmarks" > $time_f
+
+for name in 9symml_195 cycle10_2_110 inc_237 rd53_251 sqrt8_260 adr4_197 dc2_222 life_238 rd73_252 square_root_7 cm42a_207 dist_223 mlp4_245 rd84_253 urf1_149 cm82a_208 ham15_107 pm1_249 root_255 urf1_278 cm85a_209 hwb8_113 qft_10 sqn_258 urf2_277; do
     python3 modify.py --direc_root $repo_root --data_root $data_root --filename $name --oracle 'f' --depth $depth --candidates $candidates --exec_single $exec_single
-    echo $name >> result.log
-    echo $name >> time.log
-    # { time ./Duostra ../config.json >> result.log; } 2>> time.log
-    echo "" >> result.log
-    echo "" >> time.log
+    echo $name >> $result_f
+    echo $name >> $time_f
+    { time ./Duostra ../config.json >> $result_f; } 2>> $time_f
+    echo "" >> $result_f
+    echo "" >> $time_f
 done    
--- utils/run_oracle.sh
+++ utils/run_oracle.sh
@@ -9,13 +9,16 @@ depth=1
 candidates=-1
 exec_single=false
 
-echo "Results to benchmarks" > result.log
-echo "Running times to benchmarks" > time.log
+result_f=result_oracle.log
+time_f=time_oracle.log
+
+echo "Results to benchmarks" > $result_f
+echo "Running times to benchmarks" > $time_f
 for name in q17_xmg q432_xmg q499_xmg q880_xmg q1355_xmg q1908_xmg q2670_xmg q3540_xmg q5315_xmg q6288_xmg q7552_xmg; do
     python3 modify.py --direc_root $repo_root --data_root $data_root --filename $name --oracle 't' --depth $depth --candidates $candidates --exec_single $exec_single
-    echo $name >> result.log
-    echo $name >> time.log
-    { time ./Duostra ../config.json >> result.log; } 2>> time.log
-    echo "" >> result.log
-    echo "" >> time.log
+    echo $name >> $result_f
+    echo $name >> $time_f
+    { time ./Duostra ../config.json >> $result_f; } 2>> $time_f
+    echo "" >> $result_f
+    echo "" >> $time_f
 done    
