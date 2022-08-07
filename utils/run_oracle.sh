5dbaf2061c4a5a20ba2da883f6d747e4bebcbae6
chinyi0523 <james31423a@gmail.com>
2022-08-08 01:59:48 +0800

    :wheelchair: Easier to check

--- utils/run_oracle.sh
+++ utils/run_oracle.sh
@@ -9,11 +9,10 @@ depth=1
 candidates=-1
 exec_single=false
 
-result_f=result_oracle.log
-time_f=time_oracle.log
-
 echo "Results to benchmarks" > $result_f
 echo "Running times to benchmarks" > $time_f
+echo "Exec_single: $exec_single, Depth: $depth" > $result_f
+echo "Exec_single: $exec_single, Depth: $depth" > $time_f
 for name in q17_xmg q432_xmg q499_xmg q880_xmg q1355_xmg q1908_xmg q2670_xmg q3540_xmg q5315_xmg q6288_xmg q7552_xmg; do
     python3 modify.py --direc_root $repo_root --data_root $data_root --filename $name --oracle 't' --depth $depth --candidates $candidates --exec_single $exec_single
     echo $name >> $result_f
